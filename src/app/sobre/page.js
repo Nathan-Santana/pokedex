b1d00b3795aaf5850d704ba2f8a98cb41420cfc6
Nathan Santana <nathansantana@rede.ulbra.br>
2023-10-07 18:44:43 -0300

    Update page.js

--- src/app/sobre/page.js
+++ src/app/sobre/page.js
@@ -3,42 +3,20 @@ export default async function About() {
         <>
             <div className="container mt-5 justify">
                 <h1 className="text-center mb-4">History</h1>
-                <p>Essa pokédex é um projeto sem fins lucrativos, que tem como objetivo estudar e aprofundar
-                    minhas habilidades em next.js.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p>Este é um aplicativo de Pokédex construído com Next.js que utiliza a API de Pokémon para exibir informações sobre os Pokémon.
+                O aplicativo permite que os usuários pesquisem e naveguem pelos Pokémon de número 1 a 1050 e visualizem seu nome, altura, peso e tipo.
                 </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p> Este projeto é uma aplicação de um fã e não detém quaisquer direitos autorais sobre Pokémon ou suas propriedades relacionadas. Pokémon é uma marca registrada da The Pokémon Company, Nintendo, e Game Freak.
+
+<b>Este projeto é sem fins lucrativos.</b> Não estou buscando lucro com a criação ou distribuição deste aplicativo. O objetivo principal é ser um estudo prático sobre desenvolvimento Web utilizando node.js e a API oficial de Pokémon.
+
+Todas as informações sobre Pokémon, incluindo imagens, nomes, descrições e outras propriedades intelectuais, pertencem aos seus respectivos detentores de direitos autorais. Este projeto utiliza essas informações de maneira respeitosa e não comercial, com o intuito de compartilhar conhecimento e entusiasmo sobre o mundo de Pokémon.
+
+Se você é o detentor dos direitos autorais de qualquer conteúdo relacionado a Pokémon e deseja que o mesmo seja removido deste projeto, entre em contato imediatamente e tomaremi as medidas apropriadas.
+
+Agradeço a compreensão.
                 </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
-                </p>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                    when an unknown printer took a galley of type and scrambled it to make a type
-                    specimen book. It has survived not only five centuries, but also the leap into
-                    electronic typesetting, remaining essentially unchanged. It was popularised in the
-                    1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                    and more recently with desktop publishing software like Aldus PageMaker including
-                    versions of Lorem Ipsum.
+                <p> <b>Pokémon © 1996-2023 Pokémon. © 1995-2023 Nintendo/Creatures Inc./GAME FREAK inc.</b>
                 </p>
             </div>
         </>
